c0e8dea35fb99fcbf6ddfa14224068af43d0161e
AnTelegina <58634939+AnTelegina@users.noreply.github.com>
2020-02-20 04:14:21 +0500

    html parse

--- src/com/company/Jokes.java
+++ src/com/company/Jokes.java
@@ -11,22 +11,14 @@ import java.util.Random;
 
 class Jokes {
     static void make_a_joke() throws IOException {
-       // System.out.println("Joke!");
         Random random = new Random();
         ArrayList<String> jks = new ArrayList<>();
 
         Document doc = Jsoup.connect("http://www.laughfactory.com/jokes/food-jokes").get();
-        String title = doc.title();
         Elements jokes = doc.select("p[id]");
         for (Element e : jokes) {
-            //System.out.println(e.text());
             jks.add(e.text());
         }
-
-
-
-       // for (String i : jks)
-         //   System.out.println(i);
 
         System.out.println(jks.get(random.nextInt(jks.size())));
     }
